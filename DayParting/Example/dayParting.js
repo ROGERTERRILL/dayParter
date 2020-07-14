a4c5e5b06af2fc146d55b97a3bede4ebb644244f
Roger Terrill <lpaddikt@gmail.com>
2020-07-13 17:58:28 -0700

    init commit

---
+++ DayParting/Example/dayParting.js
@@ -0,0 +1,19 @@
+$(function () {
+  var isMouseDown = false;
+  $("#our_table td")
+    .mousedown(function () {
+      isMouseDown = true;
+      $(this).toggleClass("highlighted");
+      return false; // prevent text selection
+    })
+    .mouseover(function () {
+      if (isMouseDown) {
+        $(this).toggleClass("highlighted");
+      }
+    });
+  
+  $(document)
+    .mouseup(function () {
+      isMouseDown = false;
+    });
+});
